f64469ae0702f1bfac1782b314e78ced8723ab5f
Jeff Greco <jeffpgreco@gmail.com>
2019-12-20 05:25:50 -0500

    added header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+
+import React from "react";
+import "./style.css";
+
+const Header = props => (
+
+  <div className="header">
+
+    <div className="title">{props.children}</div>
+
+    <div className="score">
+
+      Score: {props.score} 
+      High Score: {props.highscore}
+
+    </div>
+
+  </div>
+
+);
+
+export default Header;
